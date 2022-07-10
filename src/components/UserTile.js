83ae79059d5c0eb6a78d21b6065056db5166b284
Marcin <marcincabaj94@gmail.com>
2022-07-10 17:32:49 +0200

    removed never used hooks

--- src/components/UserTile.js
+++ src/components/UserTile.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Image from '../images/image-jeremy.png';
 import '../styles/UserTileStyles.css';
 
-function UserTile({ handleClick, range }) {
+function UserTile({ handleClick }) {
 	const buttons = ['Daily', 'Weekly', 'Monthly'];
 
 	return (
 		<main className='userTile'>
 			<section className='sectionPerson'>
-				<img src={Image} alt='User image'></img>
+				<img src={Image} alt='User'></img>
 				<div>
 					<span>Report for</span>
 					<span className='personName'>Jeremy Robson</span>
